74e92926c133e45845f0030bba8c4234c1809ab5
antoinco <antoinco@student.21-school.ru>
2023-10-15 16:34:36 +0300

    without 4,5,13

--- src/part3.sql
+++ src/part3.sql
@@ -1,3 +1,4 @@
+-- Active: 1696963787123@@127.0.0.1@5432@info21
 -- task1
 CREATE
 OR REPLACE FUNCTION fnc_part3_task1() RETURNS TABLE (
@@ -68,6 +69,9 @@ SELECT * from fnc_part3_task3('2023-10-10');
 
 
 -- task 4
+
+
+select * from TransferredPoints;
 
 
 -- task 6
@@ -204,13 +208,123 @@ select tb1.peer1 as Peer,recommendedpeer from tb1 join tb2 on tb1.peer1 = tb2.pe
 END;
 $$
 
-drop Procedure fnc_part3_task9;
+drop Procedure fnc_part3_task8;
 
 BEGIN;
     CALL fnc_part3_task8();
     FETCH ALL FROM "pr_result_part3_task8";
 END;
 
+-- task 9
+
+
+--  Определить процент пиров, которые:
+
+-- Приступили только к блоку 1
+-- Приступили только к блоку 2
+-- Приступили к обоим
+-- Не приступили ни к одному
+
+
+ CREATE OR REPLACE PROCEDURE fnc_part3_task9(task1 VARCHAR(16), task2 VARCHAR(16),IN result REFCURSOR = 'pr_result_part3_task9')
+LANGUAGE plpgsql    
+AS $$
+DECLARE
+count_all_peers INT = (SELECT count(*) from peers);
+count_peers_task1 INT = (select count(*) from 
+(select DISTINCT checks.peer from p2p join checks on checks.id = p2p."Check" where p2p."State" = 'Start' 
+AND REGEXP_LIKE (checks.task,(select concat (task1,'[0-7]'))) group by checks.peer));
+count_peers_task2 INT = (select count(*) from 
+(select DISTINCT checks.peer from p2p join checks on checks.id = p2p."Check" where p2p."State" = 'Start' 
+AND REGEXP_LIKE (checks.task,(select concat (task2,'[0-7]'))) group by checks.peer));
+count_peers_both INT = (with tb1 as (
+select DISTINCT checks.peer from p2p join checks on checks.id = p2p."Check" where p2p."State" = 'Start' 
+AND REGEXP_LIKE (checks.task,(select concat (task1,'[0-7]'))) group by checks.peer),
+tb2 as (
+select DISTINCT checks.peer from p2p join checks on checks.id = p2p."Check" where p2p."State" = 'Start' 
+AND REGEXP_LIKE (checks.task,(select concat (task2,'[0-7]')))  group by checks.peer)
+select count(*) from tb1 join tb2 on tb1.peer = tb2.peer);
+count_peers_not_started INT = count_all_peers - count_peers_task1 - count_peers_task2 + count_peers_both;
+BEGIN
+OPEN result FOR
+select count_peers_task1 as StartedBlock1,count_peers_task2 as StartedBlock2,count_peers_both as StartedBothBlocks, count_peers_not_started as DidntStartAnyBlock;
+END;
+$$
+
+drop Procedure fnc_part3_task9;
+
+BEGIN;
+    CALL fnc_part3_task9('C','DO');
+    FETCH ALL FROM "pr_result_part3_task9";
+END;
+
+
+--- task10
+--Определить процент пиров, которые когда-либо успешно проходили проверку в свой день рождения
+
+
+CREATE OR REPLACE PROCEDURE fnc_part3_task10(IN result REFCURSOR = 'pr_result_part3_task10')
+LANGUAGE plpgsql    
+AS $$
+DECLARE
+count_peers_started INT = (select count(*) from (
+select DISTINCT peers.nickname from p2p join checks on checks.id = p2p."Check" FULL join verter on verter."Check" = checks.id
+join peers on checks.peer = peers.nickname and date_part('day', checks.date) = date_part('day', peers.birthday)
+and date_part('month', checks.date) = date_part('month', peers.birthday)
+where p2p."State" = 'Start'));
+count_failed_peers INT = (select count(*) from (
+select DISTINCT peers.nickname from p2p join checks on checks.id = p2p."Check" FULL join verter on verter."Check" = checks.id
+join peers on checks.peer = peers.nickname and date_part('day', checks.date) = date_part('day', peers.birthday)
+and date_part('month', checks.date) = date_part('month', peers.birthday)
+where (p2p."State" = 'Failure' OR verter."State" = 'Failure')));
+count_success_peers INT = count_peers_started - count_failed_peers;
+BEGIN
+OPEN result FOR
+Select ROUND(CAST (((count_success_peers::NUMERIC) * 100 / (count_peers_started::NUMERIC)) as NUMERIC),0) as SuccessfulChecks,
+ROUND(CAST (((count_failed_peers::NUMERIC) * 100 / (count_peers_started::NUMERIC)) as NUMERIC),0) as UnsuccessfulChecks;
+END;
+$$
+
+drop Procedure fnc_part3_task10;
+
+BEGIN;
+    CALL fnc_part3_task10();
+    FETCH ALL FROM "pr_result_part3_task10";
+END;
+
+
+
+-- call insert_p2p('heshi','jeratta','C1_SimpleBashUtils','Start','12:30:00');
+-- call insert_p2p('heshi','jeratta','C1_SimpleBashUtils','Success','13:00:00');
+
+-- call insert_verter('heshi','C1_SimpleBashUtils','Start','13:31:00');
+-- call insert_verter('heshi','C1_SimpleBashUtils','Failure','13:36:00');
+-- select * from p2p join checks on checks.id = p2p."Check"  LIMIT 120;
+
+-- select * from checks;
+
+-- select * from verter join checks on checks.id = verter."Check";
+
+
+-- delete from verter where verter."Check" = 55;
+
+-- update checks
+-- set date = '2023-04-20'
+-- where id = 59;
+
+-- update peers
+-- set birthday = '2005-09-13'
+-- where nickname = 'dimas'
+
+
+-- select * from peers;
+
+-- insert into xp VALUES((SELECT MAX(ID) FROM xp) + 1,58,250);
+
+-- select * from TimeTracking;
+
+-- select * from xp;
+-- insert into TimeTracking VALUES ((select max(id) from TimeTracking) + 1,'heshi','2023-04-20','13:20:00',2);
 
 -- task 11
 --  Определить всех пиров, которые сдали заданные задания 1 и 2, но не сдали задание 3
@@ -307,3 +421,112 @@ END;
 
 drop Procedure fnc_part3_task14;
 
+
+-- task 15 
+-- 15) Определить пиров, приходивших раньше заданного времени не менее N раз за всё время
+-- Параметры процедуры: время, количество раз N. 
+-- Формат вывода: список пиров
+
+-- Считаю только самый вход в кампус в течение дня
+
+
+CREATE OR REPLACE PROCEDURE fnc_part3_task15(date_cur TIME WITHOUT TIME ZONE,N INT, IN result REFCURSOR = 'pr_result_part3_task15')
+LANGUAGE plpgsql    
+AS $$
+BEGIN
+OPEN result FOR
+with tb1 as (
+select peer,"Date" from timetracking  where "state" = 1  and "Time" < date_cur GROUP BY peer,"Date")
+select peer from tb1 GROUP BY peer having count(*) >= N;
+END;
+$$ 
+
+BEGIN;
+    CALL fnc_part3_task15('20:00:00',3);
+    FETCH ALL FROM "pr_result_part3_task15";
+END;
+
+drop Procedure fnc_part3_task15;
+
+
+-- task 16 
+
+-- 16) Определить пиров, выходивших за последние N дней из кампуса больше M раз
+-- Параметры процедуры: количество дней N, количество раз M. 
+-- Формат вывода: список пиров
+
+-- Считаю абсолютно все выходы из кампуса, даже в течение дня за исключением последнего выхода за день.
+
+CREATE OR REPLACE PROCEDURE fnc_part3_task16(N INT ,M INT, IN result REFCURSOR = 'pr_result_part3_task16')
+LANGUAGE plpgsql    
+AS $$
+BEGIN
+OPEN result FOR
+with tb1 as (
+select peer,"Date",count("Date") - 1 as count_ from timetracking where state = 2 AND "Date" > (select date(NOW()) - N) 
+GROUP BY peer,"Date")
+Select peer from tb1 GROUP BY peer having sum(count_) > M;
+END;
+$$ 
+
+drop Procedure fnc_part3_task16;
+
+BEGIN;
+    CALL fnc_part3_task16(10,0);
+    FETCH ALL FROM "pr_result_part3_task16";
+END;
+
+
+-- select * from timetracking where "Date" = '2023-10-11'
+
+--  insert into TimeTracking VALUES ((select max(id) from TimeTracking) + 1,'antoinco','2023-10-11','14:53:00',2);
+
+
+-- 17) Определить для каждого месяца процент ранних входов
+-- Для каждого месяца посчитать, сколько раз люди, родившиеся в этот месяц, приходили в кампус за всё время (будем называть это общим числом входов). 
+-- Для каждого месяца посчитать, сколько раз люди, родившиеся в этот месяц, приходили в кампус раньше 12:00 за всё время (будем называть это числом ранних входов). 
+-- Для каждого месяца посчитать процент ранних входов в кампус относительно общего числа входов. 
+-- Формат вывода: месяц, процент ранних входов
+
+
+CREATE OR REPLACE PROCEDURE fnc_part3_task17( IN result REFCURSOR = 'pr_result_part3_task17')
+LANGUAGE plpgsql    
+AS $$
+BEGIN
+OPEN result FOR
+with tb1 as (
+select peer,TO_CHAR("Date", 'Month') AS "Month",count(*) from timetracking join peers on peers.nickname = timetracking.peer where date_part('month', timetracking."Date") = date_part('month',peers.birthday) 
+and "state" = 1 group by peer,"Date"),
+tb1_res as (
+select tb1."Month",sum(count) as count_ from tb1 GROUP BY tb1."Month"),
+tb2 as (
+select  peer,TO_CHAR("Date", 'Month') AS "Month",count(*) from timetracking join peers on peers.nickname = timetracking.peer where date_part('month', timetracking."Date") = date_part('month',peers.birthday) 
+and "state" = 1 and "Time" < '12:00:00' group by peer,"Date"),
+tb2_res as (
+select tb2."Month",sum(count) as count_early from tb2 GROUP BY tb2."Month")
+select tb1_res."Month",ROUND(CAST (((tb2_res.count_early::NUMERIC) * 100 / (tb1_res.count_::NUMERIC)) as NUMERIC),0) from tb1_res join tb2_res on tb1_res."Month" = tb2_res."Month";
+END;
+$$ 
+
+BEGIN;
+    CALL fnc_part3_task17();
+    FETCH ALL FROM "pr_result_part3_task17";
+END;
+
+drop Procedure fnc_part3_task16;
+
+
+
+-- insert into TimeTracking VALUES ((select max(id) from TimeTracking) + 1,'masha','2023-07-14','18:40:00',2);
+
+-- select * from peers;
+
+-- select * from timetracking 
+
+-- select *  from timetracking join peers on peers.nickname = timetracking.peer where date_part('month', timetracking."Date") = date_part('month',peers.birthday) 
+-- and "state" = 1 
+
+-- with tb1 as (
+-- select peer,TO_CHAR("Date", 'Month') AS "Month",count(*) from timetracking join peers on peers.nickname = timetracking.peer where date_part('month', timetracking."Date") = date_part('month',peers.birthday) 
+-- and "state" = 1 group by peer,"Date") 
+-- select tb1."Month",sum(count) from tb1 GROUP BY tb1."Month" 
